91057bec6b657c28528157816b66fca0f6ecda31
Vadim Afanasev <trim-agency@yandex.ru>
2023-06-20 18:26:10 +0700

    Создание проекта. Верстка окон.
    Разделение на папки, перенос файлов, создание классов, Xaml, верстка окон, создание UI элементов

---
+++ ManageStaffDBApp/ManageStaffDBApp/Model/DataWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStaffDBApp.Model
+{
+    public static class DataWorker
+    {
+        // Создать отдел
+        // Создать позицию
+        // Создать сотрудника
+        // Удалить отдел
+        // Удалить позицию
+        // Удалить сотрудника
+        // Редактировать отдел
+        // Редактировать позицию
+        // Редактировать сотрудника
+
+    }
+}
